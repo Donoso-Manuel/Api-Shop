577f27715dcdb1461f58c5ad46ed24e8181dd0f4
Manuel Donoso Alfaro <manueldonosoalfaro@gmail.com>
2025-02-18 06:00:12 -0300

    rev token

--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -2,15 +2,15 @@ const jwt = require('jsonwebtoken');
 
 const verifyAdmin = (req, res, next) => {
     try {
-        console.log('Cookies: ', req.cookies);
         const token = req.cookies.userToken;
+        console.log("token recibido", token)
         if (!token) {
             return res.status(401).json({ message: "Acceso denegado: No hay token" });
         }
 
         const secretKey = process.env.JWT_SECRET;
         const userDecode = jwt.verify(token, secretKey);
-
+        console.log("token decodificado", userDecode)
 
         if (userDecode.role !== 'admin') {
             return res.status(403).json({ message: "Acceso denegado: No tienes permisos de administrador" });
